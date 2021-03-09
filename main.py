eb76b93d7f5b9250ad02c01946b706fa3a681fac
Saharsh Nemuri <80348244+Skyward-Gaming@users.noreply.github.com>
2021-03-09 15:58:38 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('http://google.com'))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+        # navbar
+        navbar = QToolBar()
+        self.addToolBar(navbar)
+
+        back_btn = QAction('⏮️', self)
+        back_btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_btn)
+
+        forward_btn = QAction('⏩', self)
+        forward_btn.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_btn)
+
+        reload_btn = QAction('🔄', self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        home_btn = QAction('🏠', self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
+
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
+
+        self.browser.urlChanged.connect(self.update_url)
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl('https://github.com/Skyward-Gaming'))
+
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
+
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName('Skyward Browser')
+window = MainWindow()
+app.exec_()
